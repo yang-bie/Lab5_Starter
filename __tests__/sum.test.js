7e237eabac72a9ec24e8aa788c1966b43b0754a1
yang_bie <yangbie89@gmail.com>
2026-05-03 15:16:06 -0700

    fix test passing issue

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -2,7 +2,7 @@
 import { sum } from '../code-to-unit-test/sum.js';
 
 test('adds 1 + 2 to equal 3', () => {
-  expect(1 + 2).toBe(6);
+  expect(1 + 2).toBe(3);
 });
 
 test('adds 1 + 2 to equal 3', () => {
